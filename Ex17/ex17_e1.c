c4acb075a418ac88974d5c6d1b1d8f1849e17827
eleloya <ele.loya@gmail.com>
2015-11-21 09:04:48 -0800

    Solution to Extra Credit 1 from Exercise 17

--- Ex17/ex17_e1.c
+++ Ex17/ex17_e1.c
@@ -10,14 +10,12 @@
 struct Address {
   int id;
   int set;
-  char *name;
-  char *email;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
 };
 
 struct Database{
-  int max_data;
-  int max_rows;
-  struct Address **rows;
+  struct Address rows[MAX_ROWS];
 };
 
 struct Connection {
@@ -99,18 +97,14 @@ Database_write(struct Connection *conn)
 }
 
 void
-Database_create(struct Connection *conn, int max_rows, int max_data)
+Database_create(struct Connection *conn)
 {
   int i = 0;
-  conn->db->max_rows = max_rows;
-  conn->db->max_data = max_data;
-  conn->db->rows = malloc(sizeof(struct Address)*max_rows);
-  for(i=0;i<max_rows;i++) {
+  for(i=0;i<MAX_ROWS;i++) {
     // make a prototype to initialize it
     struct Address addr = {.id = i, .set = 0};
     // the just assign it
-    //conn->db->rows[i] = addr;
-    memcpy(&conn->db->rows[i], &addr, sizeof(struct Address));
+    conn->db->rows[i] = addr;
   }
 }
 
@@ -123,13 +117,11 @@ Database_set(struct Connection *conn, int id, const char *name,
 
   addr->set = 1;
   // WARNING: bug, read the "How to Break It" and fix this
-  addr->name = malloc(conn->db->max_data);
-  char *res = strncpy(addr->name, name, conn->db->max_data);
+  char *res = strncpy(addr->name, name, MAX_DATA);
   // demostrate the strncpy bug
   if(!res) die("Name copy failed",conn);
 
-  addr->email = malloc(conn->db->max_data);
-  res = strncpy(addr->email, email, conn->db->max_data);
+  res = strncpy(addr->email, email, MAX_DATA);
   if(!res) die("Email copy failed",conn);
 }
 
@@ -149,8 +141,7 @@ void
 Database_delete(struct Connection *conn, int id)
 {
   struct Address addr = {.id = id, .set = 0};
-  //conn->db->rows[id] = addr;
-  memcpy(&conn->db->rows[id], &addr, sizeof(struct Address));
+  conn->db->rows[id] = addr;
 }
 
 void
@@ -159,7 +150,7 @@ Database_list(struct Connection *conn)
   int i = 0;
   struct Database *db = conn->db;
 
-  for(i=0;i<db->max_rows;i++){
+  for(i=0;i<MAX_ROWS;i++){
     struct Address *cur = &db->rows[i];
 
     if(cur->set){
@@ -167,40 +158,39 @@ Database_list(struct Connection *conn)
     }
   }
 }
-
 int main(int argc, char *argv[])
 {
-    if(argc < 5) die("USAGE: ex17 <dbfile> <rows> <data> <action> [action params]",NULL);
+    if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]", NULL);
 
     char *filename = argv[1];
-    char action = argv[4][0];
+    char action = argv[2][0];
     struct Connection *conn = Database_open(filename, action);
     int id = 0;
 
-    if(argc > 5) id = atoi(argv[5]);
-    if(id >= atoi(argv[2])) die("There's not that many records.",conn);
+    if(argc > 3) id = atoi(argv[3]);
+    if(id >= MAX_ROWS) die("There's not that many records.", conn);
 
     switch(action) {
         case 'c':
-            Database_create(conn,atoi(argv[2]),atoi(argv[3]));
+            Database_create(conn);
             Database_write(conn);
             break;
 
         case 'g':
-            if(argc != 6) die("Need an id to get",conn);
+            if(argc != 4) die("Need an id to get", conn);
 
             Database_get(conn, id);
             break;
 
         case 's':
-            if(argc != 8) die("Need id, name, email to set",conn);
+            if(argc != 6) die("Need id, name, email to set", conn);
 
-            Database_set(conn, id, argv[6], argv[7]);
+            Database_set(conn, id, argv[4], argv[5]);
             Database_write(conn);
             break;
 
         case 'd':
-            if(argc != 6) die("Need id to delete",conn);
+            if(argc != 4) die("Need id to delete", conn);
 
             Database_delete(conn, id);
             Database_write(conn);
@@ -210,7 +200,7 @@ int main(int argc, char *argv[])
             Database_list(conn);
             break;
         default:
-            die("Invalid action, only: c=create, g=get, s=set, d=del, l=list",conn);
+            die("Invalid action, only: c=create, g=get, s=set, d=del, l=list", conn);
     }
 
     Database_close(conn);
