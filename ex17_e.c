28cc4b4e9aa47f95b07435a470f26b0e31acc6e5
eleloya <ele.loya@gmail.com>
2015-11-21 06:13:35 -0800

    Augment die function to allow conn variable

--- ex17_e.c
+++ ex17_e.c
@@ -10,12 +10,14 @@
 struct Address {
   int id;
   int set;
-  char name[MAX_DATA];
-  char email[MAX_DATA];
+  char *name;
+  char *email;
 };
 
 struct Database{
-  struct Address rows[MAX_ROWS];
+  int max_data;
+  int max_rows;
+  struct Address *rows;
 };
 
 struct Connection {
@@ -97,15 +99,18 @@ Database_write(struct Connection *conn)
 }
 
 void
-Database_create(struct Connection *conn)
+Database_create(struct Connection *conn, int max_rows, int max_data)
 {
   int i = 0;
-
-  for(i=0;i<MAX_ROWS;i++) {
+  conn->db->max_rows = max_rows;
+  conn->db->max_data = max_data;
+  conn->db->rows = malloc(sizeof(struct Address)*max_rows);
+  for(i=0;i<max_rows;i++) {
     // make a prototype to initialize it
     struct Address addr = {.id = i, .set = 0};
     // the just assign it
-    conn->db->rows[i] = addr;
+    //conn->db->rows[i] = addr;
+    memcpy(&conn->db->rows[i], &addr, sizeof(struct Address));
   }
 }
 
@@ -118,11 +123,13 @@ Database_set(struct Connection *conn, int id, const char *name,
 
   addr->set = 1;
   // WARNING: bug, read the "How to Break It" and fix this
-  char *res = strncpy(addr->name, name, MAX_DATA);
+  addr->name = malloc(conn->db->max_data);
+  char *res = strncpy(addr->name, name, conn->db->max_data);
   // demostrate the strncpy bug
   if(!res) die("Name copy failed",conn);
 
-  res = strncpy(addr->email, email, MAX_DATA);
+  addr->email = malloc(conn->db->max_data);
+  res = strncpy(addr->email, email, conn->db->max_data);
   if(!res) die("Email copy failed",conn);
 }
 
@@ -142,7 +149,8 @@ void
 Database_delete(struct Connection *conn, int id)
 {
   struct Address addr = {.id = id, .set = 0};
-  conn->db->rows[id] = addr;
+  //conn->db->rows[id] = addr;
+  memcpy(&conn->db->rows[id], &addr, sizeof(struct Address));
 }
 
 void
@@ -151,7 +159,7 @@ Database_list(struct Connection *conn)
   int i = 0;
   struct Database *db = conn->db;
 
-  for(i=0;i<MAX_ROWS;i++){
+  for(i=0;i<db->max_rows;i++){
     struct Address *cur = &db->rows[i];
 
     if(cur->set){
@@ -162,37 +170,37 @@ Database_list(struct Connection *conn)
 
 int main(int argc, char *argv[])
 {
-    if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]",NULL);
+    if(argc < 5) die("USAGE: ex17 <dbfile> <rows> <data> <action> [action params]",NULL);
 
     char *filename = argv[1];
-    char action = argv[2][0];
+    char action = argv[4][0];
     struct Connection *conn = Database_open(filename, action);
     int id = 0;
 
-    if(argc > 3) id = atoi(argv[3]);
-    if(id >= MAX_ROWS) die("There's not that many records.",conn);
+    if(argc > 5) id = atoi(argv[5]);
+    if(id >= atoi(argv[2])) die("There's not that many records.",conn);
 
     switch(action) {
         case 'c':
-            Database_create(conn);
+            Database_create(conn,atoi(argv[2]),atoi(argv[3]));
             Database_write(conn);
             break;
 
         case 'g':
-            if(argc != 4) die("Need an id to get",conn);
+            if(argc != 6) die("Need an id to get",conn);
 
             Database_get(conn, id);
             break;
 
         case 's':
-            if(argc != 6) die("Need id, name, email to set",conn);
+            if(argc != 8) die("Need id, name, email to set",conn);
 
-            Database_set(conn, id, argv[4], argv[5]);
+            Database_set(conn, id, argv[6], argv[7]);
             Database_write(conn);
             break;
 
         case 'd':
-            if(argc != 4) die("Need id to delete",conn);
+            if(argc != 6) die("Need id to delete",conn);
 
             Database_delete(conn, id);
             Database_write(conn);
