cae11c8c43d3607215700007e895fd976f3539f1
Luis <luis@loya.xyz>
2015-11-21 03:04:39 -0800

    Did the extracredtis for exercise 14

--- ex14.c
+++ ex14.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[],size_t strlength);
 
 void print_arguments(int argc, char *argv[])
 {
   int i = 0;
 
   for(i=0;i<argc;i++){
-    print_letters(argv[i]);
+    print_letters(argv[i],strlen(argv[i]));
   }
 }
 
-void print_letters(char arg[])
+void print_letters(char arg[],size_t strlength)
 {
   int i = 0;
 
-  for(i=0;arg[i]!='\0';i++){
+  for(i=0;i<strlength;i++){
     char ch = arg[i];
 
-    if(can_print_it(ch)){
+    if(isalpha(ch) || isblank(ch)){
       printf("'%c' == %d ", ch, ch);
     }
   }
   printf("\n");
-}
-
-int can_print_it(char ch)
-{
-  return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[])
